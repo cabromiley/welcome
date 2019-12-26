20ad774916dc66b0021d90203f7542ec7eaa45d6
Curtis <cabromiley@gmail.com>
2019-12-26 19:53:13 +0000

    Updated the styles for card

--- resources/views/components/skill-card.blade.php
+++ resources/views/components/skill-card.blade.php
@@ -1,3 +1,5 @@
-<a href="/skills/{{ Str::slug($skill) }}" class="block flex justify-center items-center h-64 bg-gray-100 m-4">
+<a href="/skills/{{ Str::slug($skill) }}"
+    class="block flex flex-col justify-center items-center h-64 bg-gray-100 m-4 text-gray-800 hover:text-purple-800">
     <i class="fab fa-{{ $skill ?? '' }} fa-3x"></i>
+    <span class="capitalize leading-loose">{{ $skill }}</span>
 </a>
